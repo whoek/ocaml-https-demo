fa59b361f94c47d300c7fe17096e74edd02f37fe
Willem <willem@matimba.com>
2023-02-25 18:59:34 +0000

    auto update from remote

--- demo.ml
+++ demo.ml
@@ -2,7 +2,16 @@ open Lwt
 open Cohttp
 open Cohttp_lwt_unix
 
-let body =
+let body1 =
+  Client.get (Uri.of_string "https://httpbin.org/get") >>= fun (resp, body) ->
+  let code = resp |> Response.status |> Code.code_of_status in
+  Printf.printf "Response code: %d\n" code;
+  Printf.printf "Headers: %s\n" (resp |> Response.headers |> Header.to_string);
+  body |> Cohttp_lwt.Body.to_string >|= fun body ->
+  Printf.printf "Body of length: %d\n" (String.length body);
+  body
+
+let body2 =
   Client.get (Uri.of_string "https://httpbin.org/get") >>= fun (resp, body) ->
   let code = resp |> Response.status |> Code.code_of_status in
   Printf.printf "Response code: %d\n" code;
@@ -12,7 +21,8 @@ let body =
   body
 
 let () =
-  let body = Lwt_main.run body in
-  print_endline ("Received body\n" ^ body)
-
+  let body1 = Lwt_main.run body1 in
+  let () = print_endline ("Received body\n" ^ body1) in
+  let body2 = Lwt_main.run body2 in
+  print_endline ("Received body\n" ^ body2)
   
